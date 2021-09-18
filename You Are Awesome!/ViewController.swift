054e74dd7b044963778c76518227045785abf1ef
Blake Rosenthal <brosenthal@zagmail.gonzaga.edu>
2021-09-17 19:08:39 -0700

    Chapter 2.11

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,32 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        // Message Function
-        let messages = ["You Are Awesome!", "You Are Great!","You Are Fantastic!", "Fabulous? That's You!"]
-        messageLabel.text = messages[messageNumber]
-        messageNumber = messageNumber + 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
-        
-        
-        // Image Function
-        print(imageNumber)
-        
-        let imageName = "image\(imageNumber)"
-        imageView.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the Genius Bar Needs Help, They Call You!",
+                        "Fabulous? That's You!",
+                        "You've Got The Design Skills of Jony Ive!"]
+        messageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageView.image = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
     }
     
 }
